e91f58e2290815f690d04b0674cf2ce0f6944ec7
en <45616806+theednorc@users.noreply.github.com>
2022-08-27 01:39:34 +0630

    upd

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cupertino Pane Starter
+
+Cupertino Pane by [github.com/roman-rr/cupertino-pane]
+
+Done Testing Basic Pane Successfully but need to explore more !
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+import { CupertinoPane, CupertinoSettings } from 'cupertino-pane';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  myPane: any
+  constructor(public navCtrl: NavController) {
+  }
 
-  constructor() {}
+  async ngAfterViewInit(){
+    let a = document.getElementsByClassName('cupertino-pane')
+    let settings: CupertinoSettings = { 
+      //showDraggable: true,
+      topperOverflow : true
+     };
+    
+    console.log(a);
+    this.myPane = new CupertinoPane('.cupertino-pane', settings);
+    
+    await this.myPane.present({animate: true});
+  }
+
+  openNow(){
+    this.myPane.present({animate: true});
+  }
 
 }
